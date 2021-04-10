7a118e264ddeb490b539b5ce90eeca8568830a1e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-04-10 15:26:47 -0400

    Delete portscanner.py

--- Scripts/portscanner.py
+++
@@ -1,9 +0,0 @@
-import socket as sk
-for port in range(1,1024):
-    try:
-        s=sk.socket(sk.AF_INET,sk.SOCK_STREAM)
-        s.settimeout(1000)
-        s.connect(('192.168.2.180',port))
-        print('%d:OPEN' % (port))
-        s.close
-    except: continue
