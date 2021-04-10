3a81add50aad58027bec321c96684691f6dcced9
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-04-10 15:26:42 -0400

    Delete regexsearch.py

--- Scripts/regexsearch.py
+++
@@ -1,7 +0,0 @@
-import glob, re
-for msg in glob.glob('/tmp/*.txt'):
-    filer = open((msg),'r')
-    data = filer.read()
-    message = re.findall(r'<messaage>(.*?)>/message>', data,re.DOTALL)
-    print("File %s contains %s" % (str(msg),message))
-    filer.close()
