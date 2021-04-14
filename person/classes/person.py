a8c6107b8c2689fc095a21ea6f633804753462e8
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-14 17:39:34 -0400

    commit

--- person/classes/person.py
+++ person/classes/person.py
@@ -6,17 +6,20 @@ class Person:
         """Created a method to check the input of the users age""" 
         if age.isdigit():
             print("Great, we have your age down!")
+            return True
         else:
             print("Sorry, please try again!")
     def persons_name_check(self,name):
         """Created a method to check the input of the users name""" 
         if ("." in name) and (name.replace('.','')).isalnum() or ("-" in name) and (name.replace('-','')).isalnum():
             print("Great, we have your name down!")
+            return True
         else:
             print("Sorry, please try again!")
     def persons_color_check(self,color):
         """Created a method to check the input of the users favorite color""" 
         if (" " in color) and (color.replace(' ','')).isalnum() and len(color.strip()) > 0: 
             print("Great, we have your favorite color down!")
+            return True
         else:
             print("Sorry, please try again!")
