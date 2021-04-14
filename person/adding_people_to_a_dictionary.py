73d99bd8c10a095ba3dccf3290ae4259e3deed0d
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-14 17:10:33 -0400

    commit

--- person/adding_people_to_a_dictionary.py
+++ person/adding_people_to_a_dictionary.py
@@ -1,7 +1,14 @@
 #Adding a flag
-not_done = False
+not_age_done = False
 # Asking for the persons age.
-while not not_done:
+while not not_age_done:
     print("What is your age?")
     #Allowing the user to input an age.
     persons_age = input()
+not_age_done = False
+# Asking for the persons age.
+while not not_age_done:
+    print("What is your name?")
+    #Allowing the user to input an age.
+    persons_name = input()
+    
