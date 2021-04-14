f939a0917283cc502cc22226c1817681985373f2
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-14 17:29:41 -0400

    commit

--- person/adding_players_to_a_dictionary.py
+++ person/adding_players_to_a_dictionary.py
@@ -9,6 +9,7 @@ while not not_age_done:
     print("What is your age?")
     #Allowing the user to input an age.
     persons_age = input()
+    not_age_done = new_person.persons_age_check(persons_age)
 #Setting a flag
 not_name_done = False
 # Asking for the not_name_done.
