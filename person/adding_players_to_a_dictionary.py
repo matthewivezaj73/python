5ec461fdcb9bd433a5cba94214573b50daa2f8a7
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-14 17:34:59 -0400

    commit

--- person/adding_players_to_a_dictionary.py
+++ person/adding_players_to_a_dictionary.py
@@ -1,40 +1,42 @@
-#Creating a blank list.
-list_of_people = []
-#mporting a class
-from classes.person import Person
-#Creating an instance of a person
-new_person = Person("Phil")
-#Adding a flag
-not_age_done = False
-# Asking for the persons age.
-while not not_age_done:
-    print("What is your age?")
-    #Allowing the user to input an age.
-    persons_age = input()
-    #Testing for the person's age.
-    not_age_done = new_person.persons_age_check(persons_age)
-#Setting a flag
-not_name_done = False
-# Asking for the not_name_done.
-while not not_name_done:
-    print("What is your name?")
-    #Allowing the user to input an name.
-    user_name = input()
-    #Testing for the person's name.
-    not_name_done = new_person.persons_name_check(user_name)
-#Setting a flag
-not_color_done = False
-#Testing for the favorite color
-while not not_color_done:
-    #Asking the user for their favorite color.
-    print("What is your favorite color?")
-    #Asking the user for input.
-    favorite_color = input()
-    #Testing for the person's favorite color.
-    not_color_done = new_person.persons_color_check(favorite_color)
-#If the not_color_done, not_name_done, and not_age_done are ok 
-if not_color_done and not_name_done and not_age_done:
-    #Creating the dictionary
-    person_profile = {"The persons name:":user_name,"The persons age:":persons_age, "The persons favorite color:":favorite_color}
-    #Appending each profile to the list of people.
-    list_of_people.append(person_profile)
+not_finished_yet = False
+while not not_finished_yet:
+    #Creating a blank list.
+    list_of_people = []
+    #mporting a class
+    from classes.person import Person
+    #Creating an instance of a person
+    new_person = Person("Phil")
+    #Adding a flag
+    not_age_done = False
+    # Asking for the persons age.
+    while not not_age_done:
+        print("What is your age?")
+        #Allowing the user to input an age.
+        persons_age = input()
+        #Testing for the person's age.
+        not_age_done = new_person.persons_age_check(persons_age)
+    #Setting a flag
+    not_name_done = False
+    # Asking for the not_name_done.
+    while not not_name_done:
+        print("What is your name?")
+        #Allowing the user to input an name.
+        user_name = input()
+        #Testing for the person's name.
+        not_name_done = new_person.persons_name_check(user_name)
+    #Setting a flag
+    not_color_done = False
+    #Testing for the favorite color
+    while not not_color_done:
+        #Asking the user for their favorite color.
+        print("What is your favorite color?")
+        #Asking the user for input.
+        favorite_color = input()
+        #Testing for the person's favorite color.
+        not_color_done = new_person.persons_color_check(favorite_color)
+    #If the not_color_done, not_name_done, and not_age_done are ok 
+    if not_color_done and not_name_done and not_age_done:
+        #Creating the dictionary
+        person_profile = {"The persons name:":user_name,"The persons age:":persons_age, "The persons favorite color:":favorite_color}
+        #Appending each profile to the list of people.
+        list_of_people.append(person_profile)
