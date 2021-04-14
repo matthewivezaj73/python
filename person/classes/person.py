61aa2aa8b2ce2a4e9c23506c9a3ae48609f86da4
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-14 17:23:59 -0400

    commit

--- person/classes/person.py
+++ person/classes/person.py
@@ -2,7 +2,13 @@ class Person:
     def __init__(self,name=""):
         """Initializing the person's name"""
         self.name = name
-    def persons_age_check(self,name):
+    def persons_age_check(self,age):
+        """Created a method to check the input of the users age""" 
+        if age.isdigit():
+            print("Great, we have your age down!")
+        else:
+            print("Sorry, please try again!")
+    def persons_name_check(self,name):
         """Created a method to check the input of the users age""" 
         if ("." in name) and (name.replace('.','')).isalnum() or ("-" in name) and (name.replace('-','')).isalnum():
             print("Great, we have your name down!")
