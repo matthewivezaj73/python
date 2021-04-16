b29a0b051c8bf341a571f746ff1284299561cd8a
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-16 07:45:06 -0400

    commit

--- lucy/classes/lucy.py
+++ lucy/classes/lucy.py
@@ -3,3 +3,6 @@ class Lucy:
         """Initializing the name and age attributes"""
         self.name = name
         self.age = age
+    def strike(self):
+        """Created a method to indicate that Lucy performed a strike."""
+        
