15db5fffa3c4d32ba9954e9c5782aaf587414183
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-16 07:44:02 -0400

    commit

---
+++ lucy/classes/lucy.py
@@ -0,0 +1,5 @@
+class Lucy:
+    def __init__(self,name,age):
+        """Initializing the name and age attributes"""
+        self.name = name
+        self.age = age
---
+++ lucy/lucy_main.py
@@ -0,0 +1,1 @@
+#Importing the Lucy class
