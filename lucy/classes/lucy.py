ba7a25449010e61cb9703e306ffd2b344328f7cd
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-16 07:45:36 -0400

    commit

--- lucy/classes/lucy.py
+++ lucy/classes/lucy.py
@@ -5,4 +5,4 @@ class Lucy:
         self.age = age
     def strike(self):
         """Created a method to indicate that Lucy performed a strike."""
-        
+        which_strike = input("Which blade will you be using to perform this strike?")
