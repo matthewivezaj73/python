c17ad260835a31ac4a067017574f5eaa634bfcfa
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-16 07:47:50 -0400

    commit

--- lucy/lucy_main.py
+++ lucy/lucy_main.py
@@ -1,1 +1,13 @@
 #Importing the Lucy class
+
+
+#Setting a flag
+not_done = False
+#Testing for the dialogue
+while not not_done:
+    #Asking the user which blade they will be using
+    which_strike = input("Which blade will you be using to perform this strike?")
+    if which_strike.isalpha():
+        print("Arrrgghhhhh!!!!!")
+    else:
+        print("Shoot, it missed...")
