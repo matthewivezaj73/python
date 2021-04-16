ddd64580967d789f9e7d65e20159552296bcceb0
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-04-16 07:55:43 -0400

    commit

--- lucy/lucy_main.py
+++ lucy/lucy_main.py
@@ -1,13 +1,14 @@
 #Importing the Lucy class
-from lucy.classes.lucy_class import Lucy
-
+from classes.lucy_class import Lucy
+new_lucy = Lucy("Lucy",23)
 #Setting a flag
 not_done = False
 #Testing for the dialogue
 while not not_done:
     #Asking the user which blade they will be using
     which_strike = input("Which blade will you be using to perform this strike?")
-    if which_strike:
-        print("Arrrgghhhhh!!!!!")
-    else:
-        print("Shoot, it missed...")
+    #Setting a flag
+    strike_done = False
+    while not strike_done:
+        #Testing the strike.
+        strike_done = new_lucy.strike(which_strike)
