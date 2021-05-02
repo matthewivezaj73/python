496c7b2123c365fb7b1389d5eafe8807fcd0c73e
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-05-02 19:44:16 -0400

    commit

--- login_screen/classes/login_class.py
+++ login_screen/classes/login_class.py
@@ -3,4 +3,10 @@ class Login:
     Created a class to handle when a user is logging into a system
     """
     def __init__(self,name,date,time):
-        """Initializing the following attributes in the class"""
+        """
+        Initializing the following attributes in the class
+        """
+        self.name = name
+        self.date = date
+        self.time = time
+        
