92a51fc8aed3f98d709fbfa6df5250af58bf0ec8
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-05-02 19:51:50 -0400

    commit

--- login_screen/classes/login_class.py
+++ login_screen/classes/login_class.py
@@ -9,6 +9,9 @@ class Login:
         self.name = name
         self.date = date
         self.time = time
-    def check_name(self):
+    def check_name(self,username):
         """Created a method to verify the user name"""
-        
+        if (("." in username) and (username.replace('.','')).isalpha()) or (("-" in username) and (username.replace('.','')).isalpha()) or (("." and "-" in username) and (username.replace('.','').replace('-','')).isalpha()) or username.isalpha():
+            return True
+        else:
+            return False
