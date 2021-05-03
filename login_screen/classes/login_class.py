7fc276ca2d10ce09e0ac5ad1c8ff98e3cf67ff42
mi0179932 <78231671+mi0179932@users.noreply.github.com>
2021-05-02 20:00:27 -0400

    commit

--- login_screen/classes/login_class.py
+++ login_screen/classes/login_class.py
@@ -1,18 +1,25 @@
 class Login:
     """
-    Created a class to handle when a user is logging into a system
+    Created a class to handle when a user is logging into a system.
     """
     def __init__(self,name,date,time):
         """
-        Initializing the following attributes in the class
+        Initializing the following attributes in the class.
         """
         self.name = name
         self.date = date
         self.time = time
     def check_name(self,username):
-        """Created a method to verify the user name"""
+        """
+        Created a method to verify the user name.
+        """
         if (("." in username) and (username.replace('.','')).isalpha()) or (("-" in username) and (username.replace('.','')).isalpha()) or username.isalpha():
             return True
         else:
             return False
-    
+    def check_password(self,password):
+        """
+        Creating a method to verify a password.
+        """
+        if len(password) > 0:
+            
